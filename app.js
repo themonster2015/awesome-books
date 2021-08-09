648083892c7d6537c19afd0ec82201baeebe1ee5
Victor Otieno <victorotieno598@gmail.com>
2021-08-09 16:50:54 +0300

    Add create books function

--- app.js
+++ app.js
@@ -5,20 +5,34 @@ let booksList = document.getElementById('booksList');
 
 
 let books = [
-    { title: 'Book One', author: 'James' },
     { title: 'Book One', author: 'James' }
 ];
 
+addBtn.addEventListener('click', function (e){
+    let data = { title: title.value, author: author.value };
+    if (data.title && data.author) {
+        
+        books.unshift(data)
+        booksList.innerHTML = ''
+        display();
+
+        title.value = ''
+        author.value = ''
+
+    } else {
+        alert('Title or Author cannot be blank!')
+    }
+});
+
 function display() {
     books.forEach(val => {
-
-        console.log(val)
 
         let li = document.createElement('li');
         let text = `<div>
             <strong>${val.title}</strong><br>
             <small>${val.author}</small>
-        </div><br>`;
+            </div><br>
+        `;
 
         li.innerHTML= text;
         booksList.appendChild(li);
